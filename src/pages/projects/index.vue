6028101dedd72c86979910984e2c62964566b54c
= <er.carlos.albornoz@gmail.com>
2024-07-28 14:28:14 -0600

    feat: add first fetching logic and db types

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,6 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { supabase } from '@/lib/supabaseClient';
+import type { Tables } from 'database/types';
+
+const projects = ref<Tables<'projects'>[] | null>();
+
+(async () => {
+    const { data, error } = await supabase.from('projects').select()
+
+    if (error) console.log(error)
+
+    projects.value = data;
+
+    console.log({ projects: data })
+})()
+</script>
 <template>
     <div>
-        <h1>Projects View</h1>
-        <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to porject 1</RouterLink>
+        <h1>List of projects</h1>
+        <ul>
+            <li v-for="project in projects" :key="project.id">
+                {{ project.name }}
+            </li>
+        </ul>
     </div>
 </template>
