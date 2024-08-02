570890c8e8d1311e9a767dde48e28db1b88b1bd6
= <er.carlos.albornoz@gmail.com>
2024-08-02 16:25:06 -0600

    feat: implement simple nav-bar
    - Installed Shadcn components
    - Using iconify icons

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,40 @@
+<script setup lang="ts">
+import Input from '@/components/ui/input/Input.vue'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+</script>
 <template>
-  <RouterView />
+  <nav class="flex h-16 border-b bg-muted/40 justify-between px-6 items-center">
+    <form class="w-full h-fit max-w-96 relative">
+      <iconify-icon class="absolute top-[50%] translate-y-[-50%] text-muted-foreground"
+        icon="lucide:search"></iconify-icon>
+      <Input type="text" class="w-full pl-8 bg-background" placeholder="Search..." />
+    </form>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
