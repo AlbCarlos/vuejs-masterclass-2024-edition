a16b8579c3bc770185dbddc522467e47b7f1f4b2
= <er.carlos.albornoz@gmail.com>
2024-08-03 16:43:58 -0600

    feat: update projects to have a table

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,27 +1,45 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { h, ref } from 'vue';
+import type { ColumnDef } from '@tanstack/vue-table';
 import { supabase } from '@/lib/supabaseClient';
 import type { Tables } from 'database/types';
+import DataTable from '@/components/ui/data-table/DataTable.vue';
 
 const projects = ref<Tables<'projects'>[] | null>();
 
 (async () => {
-    const { data, error } = await supabase.from('projects').select()
+  const { data, error } = await supabase.from('projects').select()
 
-    if (error) console.log(error)
+  if (error) console.log(error)
 
-    projects.value = data;
-
-    console.log({ projects: data })
+  projects.value = data;
 })()
+
+const projectsColumns: ColumnDef<Tables<'projects'>>[] = [
+  {
+    accessorKey: 'name',
+    header: () => h('div', { class: 'text-left' }, 'Name'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('name'))
+    },
+  },
+  {
+    accessorKey: 'status',
+    header: () => h('div', { class: 'text-left' }, 'Status'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+    },
+  },
+  {
+    accessorKey: 'collaborators',
+    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, JSON.stringify(row.getValue('collaborators')))
+    },
+  },
+]
+
 </script>
 <template>
-    <div>
-        <h1>Projects Page</h1>
-        <ul>
-            <li v-for="project in projects" :key="project.id">
-                {{ project.name }}
-            </li>
-        </ul>
-    </div>
+  <DataTable v-if="projects" :columns="projectsColumns" :data="projects" />
 </template>
